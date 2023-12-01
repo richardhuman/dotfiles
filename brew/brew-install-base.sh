347284e66971b4c9116723869a094a87ebabad91
Richard Human <rhuman@gmail.com>
2023-12-01 14:24:28 +0200

    Add 1password cli

--- brew/brew-install-base.sh
+++ brew/brew-install-base.sh
@@ -3,20 +3,22 @@
 
 
 MAC_UTILITIES="smartmontools"
-UTILITIES="bat jq htop glances bandwhich fzf wget nmap"
-DEV_UTILITIES="nvm python3 tilt tfenv rbenv gh postgresql@16"
+UTILITIES="bat jq htop glances bandwhich fzf wget nmap 1password-cli"
+DEV_UTILITIES="nvm python3 tilt tfenv rbenv gh postgresql@16 awscli jq redis"
+TSE_DEV_UTILITIES="wkhtmltopdf"
 
 echo "> Installing BASE utilities: ${UTILITIES}"
-
 brew install ${UTILITIES}
 
 echo "> Installing DEV utilities: ${DEV_UTILITIES}"
-
 brew install ${DEV_UTILITIES}
 
 echo "> Installing MAC utilities: ${MAC_UTILITIES}"
-
 brew install ${MAC_UTILITIES}
+
+echo "> Installing TSE DEV utilities: ${TSE_DEV_UTILITIES}"
+brew install ${TSE_DEV_UTILITIES}
+
 
 # Java?
 echo "> Install sdkman"
