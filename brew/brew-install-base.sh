cdeaeffdcbd6cdf6f94e222c1efecc97208c9e5e
Richard Human <rhuman@gmail.com>
2024-03-04 18:07:39 +0200

    Updates including auto load ruby

--- brew/brew-install-base.sh
+++ brew/brew-install-base.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-
+# Things played with: dive
 
 MAC_UTILITIES="smartmontools"
 UTILITIES="bat jq htop glances bandwhich fzf wget nmap 1password-cli"
 DEV_UTILITIES="nvm python3 tilt tfenv rbenv gh postgresql@16 awscli jq redis"
-TSE_DEV_UTILITIES="wkhtmltopdf"
+# TSE_DEV_UTILITIES="wkhtmltopdf"
 
 echo "> Installing BASE utilities: ${UTILITIES}"
 brew install ${UTILITIES}
@@ -16,8 +16,13 @@ brew install ${DEV_UTILITIES}
 echo "> Installing MAC utilities: ${MAC_UTILITIES}"
 brew install ${MAC_UTILITIES}
 
-echo "> Installing TSE DEV utilities: ${TSE_DEV_UTILITIES}"
-brew install ${TSE_DEV_UTILITIES}
+# echo "> Installing TSE DEV utilities: ${TSE_DEV_UTILITIES}"
+# brew install ${TSE_DEV_UTILITIES}
+
+
+# Fonts
+brew tap homebrew/cask-fonts
+brew install font-monaspace
 
 
 # Java?
