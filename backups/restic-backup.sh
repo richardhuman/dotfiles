e0b8eac254634623131435a83b02feb62fac024b
Richard Human <rhuman@gmail.com>
2025-08-17 10:52:22 +0200

    Restic backup export vars

--- backups/restic-backup.sh
+++ backups/restic-backup.sh
@@ -1,10 +1,11 @@
-!#/bin/bash
+#!/bin/bash
+set -e
 
 # Setup
 # restic init --repo /Volumes/Richard-Mini-Backups/richard-mini-restic
 
-RESTIC_REPO=/Volumes/Richard-Mini-Backups/richard-mini-restic
-RESTIC_PASSWORD=$(op item get Mac-Mini-Restic-Backups --vault Work --fields=password --reveal)
+export RESTIC_REPO=/Volumes/Richard-Mini-Backups/richard-mini-restic
+export RESTIC_PASSWORD=$(op item get Mac-Mini-Restic-Backups --vault Work --fields=password --reveal)
 
 restic -r ${RESTIC_REPO} --verbose backup /Volumes/Data
 restic -r ${RESTIC_REPO} --verbose backup /Users/richard --exclude node_modules --exclude venv
